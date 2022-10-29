a0e06012a24f2e38e512d84c8f5bc9f8088628a7
Jordan Lee <jordan@ascendsalesforceconsulting.com>
2022-10-30 01:48:27 +0300

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,39 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-});
+})
 
 module.exports = {
   siteMetadata: {
-    title: "Vimvala",
-    titleTemplate: "%s | Vimvala",
-    description: "Experienced Salesforce Specialists who put your company's needs first.",
+    title: 'Vimvala',
+    titleTemplate: '%s | Vimvala',
+    description:
+      "Experienced Salesforce Specialists who put your company's needs first.",
   },
   plugins: [
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST
+        host: process.env.CONTENTFUL_HOST,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Vimvala',
+        short_name: 'Vimvala',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'static/vimvala-favicon.png',
       },
     },
   ],
-};
+}
