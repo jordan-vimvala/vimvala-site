f7b936a415eff3fed169da26bdab186f52d149db
Jordan Lee <jordan@ascendsalesforceconsulting.com>
2022-10-30 01:49:26 +0300

    Start navigation modifications

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,27 +1,73 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 import * as styles from './navigation.module.scss'
+import { GatsbyImage } from 'gatsby-plugin-image'
 
-const Navigation = () => (
-  <nav role="navigation" className={styles.container} aria-label="Main">
-    <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
-    </Link>
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/" activeClassName="active">
-          Home
-        </Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/" activeClassName="active">
-          Blog
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+const Navigation = () => {
+  return (
+    <div className={styles.stickyHeader}>
+      <div className={styles.container}>
+        <div className="align-items-center">
+          <div className="col-lg-3 col-md-6 col-8 d-flex align-items-center">
+            <div className="logo">
+              <Link href="/">
+                <StaticQuery
+                  query={pageQuery}
+                  render={(data) => (
+                    <GatsbyImage
+                      image={data.allContentfulAsset.nodes[0].gatsbyImageData}
+                      alt="logo"
+                    />
+                  )}
+                />
+              </Link>
+            </div>
+          </div>
+          <div id="right-header" className="col-lg-9 col-md-6 col-4">
+            <nav>
+              <ul>
+                <li className={styles.menuItem}>
+                  <Link to="/" activeClassName={styles.active}>
+                    Home
+                  </Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link to="/services" activeClassName={styles.active}>
+                    Services
+                  </Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link to="/articles" activeClassName={styles.active}>
+                    Articles
+                  </Link>
+                </li>
+                <li className={styles.menuItem}>
+                  <Link to="/contact" activeClassName={styles.active}>
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Navigation
+
+export const pageQuery = graphql`
+  query NavigationQuery {
+    allContentfulAsset(
+      filter: { contentful_id: { eq: "mEDxMfC7Rh4N70yqJClW7" } }
+    ) {
+      nodes {
+        title
+        description
+        gatsbyImageData(layout: FIXED, width: 400, placeholder: BLURRED)
+      }
+    }
+  }
+`
