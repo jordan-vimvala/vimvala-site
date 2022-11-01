5042041e0b1e3e2992e0fa4055d0f50781e43409
Jordan Lee <jordan@ascendsalesforceconsulting.com>
2022-11-01 14:49:51 +0200

    Update hero design

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -12,16 +12,16 @@ const Hero = ({ image, content, callout, overlayOpacity }) => (
     )}
     {overlayOpacity && (
       <div
-        className={`absolute top-0 w-full h-full bg-black opacity-${overlayOpacity}`}
+        className={`bg-black/${overlayOpacity} absolute top-0 w-full h-full`}
       />
     )}
     <div className="absolute top-0 w-full h-full flex flex-col items-center justify-center">
-      <div class="w-full mx-3 mt-16 md:w-2/3 md:mx-0">
+      <div className="w-full mx-3 mt-16 md:w-2/3 md:mx-0">
         <h1 className="text-xl md:text-6xl text-white text-center">{content}</h1>
       </div>
       {callout && (
         <div className="md:mt-6 mt-2">
-          <button className="bg-orange-600 text-l md:text-2xl text-white py-2 px-3 rounded ring-1 ring-slate-300">
+          <button className="bg-orange-600 text-l md:text-2xl text-white py-2 px-3 rounded">
             {callout}
           </button>
         </div>
