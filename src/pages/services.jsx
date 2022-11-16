59a44a553036f6878cc2366fd39f65e4557b7b11
Jordan Lee <jordan@ascendsalesforceconsulting.com>
2022-11-16 19:01:05 +0200

    Add top section of services page

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,26 +1,58 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
 
 import Layout from '../layout'
-import Hero from '../components/hero'
-import ArticlePreview from '../components/article-preview'
 import Seo from '../seo'
+
+import Container from '../components/container'
+
+import { FaLaptopCode, FaHeadset, FaSyncAlt } from 'react-icons/fa'
 
 class RootIndex extends React.Component {
   render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-    const [heroImage] = get(this, 'props.data.allContentfulAsset.nodes')
-
     return (
       <Layout location={this.props.location}>
-        <Hero
-          image={heroImage}
-          content={'"To Blaze New Trails for Businesses and Customers."'}
-          callout="Book Free Consulting Call"
-          overlayOpacity={30}
-        />
-        <ArticlePreview posts={posts} />
+        <Container>
+          <div className="text-center max-w-2xl mx-auto mt-32">
+            <h3 className="font-bold text-3xl mb-8">
+              Services offered to help your business
+            </h3>
+            <p>
+              Whether you're just starting in Salesforce or you want to squeeze
+              a little more functionality out of it. We're here to help you
+              utilize it to its fullest potential.
+            </p>
+          </div>
+          <div className="flex flex-col mx-auto py-12 gap-x-12 md:flex-row md:px-32">
+            <div className="flex mb-8">
+              <div className="mr-8">
+                <FaSyncAlt className="text-5xl text-primary" />
+              </div>
+              <span>
+                <strong className="block mb-1">Implementations</strong>
+                <p>Translate business requirements into automated processes.</p>
+              </span>
+            </div>
+            <div className="flex mb-8">
+              <div className="mr-8">
+                <FaHeadset className="text-5xl text-primary" />
+              </div>
+              <span>
+                <strong className="block mb-1">Consulting</strong>
+                <p>Find the best approach to solving your problem.</p>
+              </span>
+            </div>
+            <div className="flex mb-8">
+              <div className="mr-8">
+                <FaLaptopCode className="text-5xl text-primary" />
+              </div>
+              <span>
+                <strong className="block mb-1">Custom Development</strong>
+                <p>Visualforce Pages, Apex Code, Apex Triggers.</p>
+              </span>
+            </div>
+          </div>
+          {/* Add 1Up component */}
+        </Container>
       </Layout>
     )
   }
@@ -31,37 +63,3 @@ export default RootIndex
 export const Head = () => {
   return <Seo title="Services" />
 }
-
-export const pageQuery = graphql`
-  query ServicesQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      nodes {
-        title
-        slug
-        publishDate(formatString: "MMM Do, YYYY")
-        tags
-        heroImage {
-          gatsbyImageData(
-            layout: FULL_WIDTH
-            placeholder: BLURRED
-            width: 424
-            height: 212
-          )
-        }
-        description {
-          raw
-        }
-      }
-    }
-    allContentfulAsset(
-      filter: { contentful_id: { eq: "29wGjnKfMAUhvILsWZjiKd" } }
-    ) {
-      nodes {
-        title
-        description
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1920)
-        contentful_id
-      }
-    }
-  }
-`
