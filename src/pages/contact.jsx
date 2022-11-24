8166ee869c0f3295a8298b694ffaf4822d5447ea
Jordan Lee <jordan@ascendsalesforceconsulting.com>
2022-11-24 19:09:27 +0200

    Add contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import Layout from '../layout'
+import Seo from '../seo'
+
+class RootIndex extends React.Component {
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <div class="max-w-screen-md mx-auto p-5">
+          <div class="text-center mb-16">
+            <p class="mt-4 text-sm leading-7 text-slate-500 font-regular uppercase">
+              Contact
+            </p>
+            <h3 class="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-slate-900">
+              Get In <span class="text-primary">Touch</span>
+            </h3>
+          </div>
+
+          <form
+            class="w-full"
+            method="post"
+            action="https://getform.io/f/19c7479b-99af-444b-9a77-2be10547457d"
+          >
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-slate-700 text-xs font-bold mb-2"
+                  htmlFor="first-name"
+                >
+                  First Name
+                </label>
+                <input
+                  class="block w-full bg-slate-200 text-slate-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="first-name"
+                  type="text"
+                  name="first-name"
+                />
+              </div>
+              <div class="w-full md:w-1/2 px-3">
+                <label
+                  class="block uppercase tracking-wide text-slate-700 text-xs font-bold mb-2"
+                  htmlFor="last-name"
+                >
+                  Last Name
+                </label>
+                <input
+                  class="block w-full bg-slate-200 text-slate-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="last-name"
+                  type="text"
+                  name="last-name"
+                />
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label
+                  class="block uppercase tracking-wide text-slate-700 text-xs font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email Address
+                </label>
+                <input
+                  class="block w-full bg-slate-200 text-slate-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                />
+              </div>
+            </div>
+
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label
+                  class="block uppercase tracking-wide text-slate-700 text-xs font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Your Message
+                </label>
+                <textarea
+                  rows="10"
+                  class="block w-full bg-slate-200 text-slate-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  name="message"
+                ></textarea>
+              </div>
+              <div class="flex justify-between w-full px-3">
+                <button
+                  class="shadow bg-primary focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+                  type="submit"
+                >
+                  Send Message
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default RootIndex
+
+export const Head = () => {
+  return <Seo title="Contact Us" />
+}
