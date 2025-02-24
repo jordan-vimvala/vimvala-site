fddde35006f0d9738305912bc24c7ae29bdd8c88
Jordan Lee <jordan@vimvala.com>
2025-02-24 13:20:37 -0700

    Update copyright year in footer to 2024

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import Container from './container'
 const Footer = () => (
   <Container as="footer">
     <div className="text-right p-6 mt-6 text-sm border-t border-t-slate-200">
-      Copyright Vimvala &#169; 2022
+      Copyright Vimvala &#169; 2024
     </div>
   </Container>
 )
